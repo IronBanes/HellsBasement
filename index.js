df3d0eb2389983ce3582ee1ad5916048585f4d6d
IronBanes <gavinator0000@gmail.com>
2020-02-25 18:28:42 -0600

    Update index.js
    Made it so it checks if player is online had to stop for now to work on other projects

--- index.js
+++ index.js
@@ -14,10 +14,35 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
+  let prefix = process.env.PREFIX;// Sets the prefix that the bot will use to execute commands
+  let msgarray = msg.content.split(" ");//splits the message into two parts the command and the args later on
+  let cmd = msgarray[0]; // This is the command part of the message
+  let args = msgarray[1]; // this is the part of the command that tags people later on
+  let DamnedRole = msg.guild.roles.find(role => role.name === "damned");
+  let mentionmember = msg.mentions.members.first();
+  onlineMembers = msg.guild.members.filter(member => member.presence.status !== "offline");
+  
+  if(cmd === `${prefix}hell`){
+    
+
+    /*if(msg.channel.name==="hell"){
+      msg.reply("Welcome to Hell")
+    }else{
+      msg.reply("This is the wrong channel");
+    }*/
+
+  }
+  
+  
+  
+  console.log("cmd:" + cmd);
+  console.log("args:" + args);
+  console.log(prefix);
+
+
+
+
   if (msg.content === `${PREFIX}hell`) { //~hell command doesn't work at the moment big sad
-    let DamnedRole = msg.guild.roles.find(role =>role.name==="damned");
-    let member = msg.mentions.members.first();
-    member.addRole(DamnedRole).catch(console.error);
-    msg.channel.send('pong');
+    
   }
 });
