b78051429be256558166d5cb2d7487a6c639f949
IronBanes <gavinator0000@gmail.com>
2020-02-26 21:59:10 -0600

    So i made some stuff work but broke a good bit so... that sucks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN; //sets the Token for the bot to link to
 const PREFIX = process.env.PREFIX;// Sets the prefix that the bot will use to execute commands
+let Damnedmembers = [];
 
 bot.login(TOKEN);
 
@@ -20,15 +21,43 @@ bot.on('message', msg => {
   let args = msgarray[1]; // this is the part of the command that tags people later on
   let DamnedRole = msg.guild.roles.find(role => role.name === "damned");
   let mentionmember = msg.mentions.members.first();
-  onlineMembers = msg.guild.members.filter(member => member.presence.status !== "offline");
-  
-  if(cmd === `${prefix}hell`){
-    /*if(msg.channel.name==="hell"){
-      msg.reply("Welcome to Hell")
-    }else{
-      msg.reply("This is the wrong channel");
-    }*/
-  }
+  let onlineMembers = msg.guild.members.filter(member => member.presence.status !== "offline");
+  let mentionmembercut = mentionmember.toString().substring(2);
+  let mentionmemberadded = "<" &mentionmembercut;
+  console.log("IMPORTANT" );
+  console.log(onlineMembers.values());
+  console.log(mentionmembercut);
+  console.log(onlineMembers.forEach(id,mentionmemberadded));
+
+
+
+  /*if(cmd === `${prefix}hell`){
+    if(msg.channel.name==="hell")
+    {
+      if(onlineMembers.equals(mentionmemberadded))
+      {
+        if(!Damnedmembers.includes(mentionmember))
+        {
+          mentionmember.addRole(DamnedRole);
+        }
+        else
+        {
+          msg.reply("That person is already in the game.");
+        }  
+      }
+      else
+      {
+        msg.reply("That person is not online please select another user.");
+      }
+    }
+    else
+    {
+      msg.reply("failed")
+    }
+    
+
+
+  }*/
   
   
   
